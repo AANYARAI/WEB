38f87675f4cab45db3535148f2dd3440e8ee03e1
AANYARAI <aanyarai223@gmail.com>
2025-01-27 19:48:32 +0530

    js

---
+++ assignment/assignment7.js
@@ -0,0 +1,19 @@
+let arr=[
+    "https://images.unsplash.com/photo-1542664408056-8ec64f24de47?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+   "https://images.unsplash.com/photo-1476673160081-cf065607f449?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+   "https://images.unsplash.com/photo-1538941214074-9b578e573632?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+ "https://images.unsplash.com/photo-1737835602453-46cdbc7e1e3d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1737555070365-cb948afc334c?q=80&w=2049&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1736890729709-4443ac5b6bf0?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+   
+]
+
+let imageEl = document.querySelector('img');
+
+let num =0;
+setInterval(function(){
+    imageEl.setAttribute('src',arr[num]);
+    num = (num+1)% arr.length;
+},1000);
+
+
---
+++ lect27/index.js
@@ -0,0 +1,15 @@
+// let h1= document.querySelector('h1')
+// // let h2= document.querySelectorAll('h1')
+// h1.innerText='Aanya'
+// console.log(h1,"kya");
+
+// let a= document.querySelector('a')
+// a.setAttribute('href','http://youtube.come')
+
+// console.log(a.getAttribute('href'));
+
+let div= document.querySelector('div')
+let h1= document.createElement('h1')
+h1.innerText='AANYA'
+div.appendChild(h1)
+
