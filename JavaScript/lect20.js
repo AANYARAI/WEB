314d1abc3288b7c60623e652fa82990e2c6176a8
AANYARAI <aanyarai223@gmail.com>
2025-06-14 21:15:09 +0530

    DOM

---
+++ JavaScript/lect20.js
@@ -0,0 +1,39 @@
+      //DOM DOCUMENT OBJECT MODEL//
+        //HTML + JS//
+
+        // console.log(hello);
+
+        // let aanya    = document.getElementById("one")
+        // let h11 =document.getElementsByClassName("two")
+        // console.log(aanya);
+        // console.log(h11);
+
+        // let  h1 = document.getElementById("one")
+        // let h1 =document.getElementsByClassName("two")
+        // let h1 = document.querySelector(".two")
+        //       let h1 = document.querySelectorAll(".two")
+        // console.log(h1);
+        
+    
+        // for(let i of h1){
+        //     i.style.color='beige'
+        //     i.style.backgroundColor='pink'
+        // }   
+        // h1[0].style.backgroundColor='Pink'
+        // h1[0].innerText="AANYA"
+        // console.log(h1[0].innerText);
+
+        // let a = document.querySelector('a')
+        // console.log(a.getAttribute('href'));
+        // a.setAttribute("href" , "http://amazon.com")
+        
+        
+
+
+
+                const h1= document.createElement('h1')
+                const div = document.querySelector('div')
+                h1.innerText="AANYA"
+                div.append(h1)
+                console.log(div);
+                
